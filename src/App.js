ba79fc4e00d3fbbd8a08689815bdf9e8b0ecc5e6
Alfiado Ngana <alfiadoprojectos@gmail.com>
2020-02-18 15:27:35 +0200

    estilizando pagina navegar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import { GlobalStyle } from './styles/global';
 import SideBar from './components/SideBar';
 import Player from './components/Player';
 import Header from './components/Header';
+
 import { Wrapper, Container, Content } from './styles/components';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
       <Wrapper>
         <Container>
           <SideBar />
           <Content>
             <Header />
+            <Routes />
           </Content>
         </Container>
         <Player />
       </Wrapper>
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/browse/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import { Container, Title, List, PlayList } from './styles';
+
+const Browse = () => (
+  <Container>
+    <Title>Navegar</Title>
+
+    <List>
+      <PlayList>
+        <img
+          src="https://dopemusicpromotions.com/wp-content/uploads/2019/06/Stargroves-album-cover.png"
+          alt="playList"
+        />
+        <strong>Rock dos Bons</strong>
+        <p>
+          Relaxa equanto voce programa ouvindo apenas as melhores do rock
+          mundial
+        </p>
+      </PlayList>
+      <PlayList>
+        <img
+          src="https://dopemusicpromotions.com/wp-content/uploads/2019/06/Stargroves-album-cover.png"
+          alt="playList"
+        />
+        <strong>Rock dos Bons</strong>
+        <p>
+          Relaxa equanto voce programa ouvindo apenas as melhores do rock
+          mundial
+        </p>
+      </PlayList>
+      <PlayList>
+        <img
+          src="https://dopemusicpromotions.com/wp-content/uploads/2019/06/Stargroves-album-cover.png"
+          alt="playList"
+        />
+        <strong>Rock dos Bons</strong>
+        <p>
+          Relaxa equanto voce programa ouvindo apenas as melhores do rock
+          mundial
+        </p>
+      </PlayList>
+      <PlayList>
+        <img
+          src="https://dopemusicpromotions.com/wp-content/uploads/2019/06/Stargroves-album-cover.png"
+          alt="playList"
+        />
+        <strong>Rock dos Bons</strong>
+        <p>
+          Relaxa equanto voce programa ouvindo apenas as melhores do rock
+          mundial
+        </p>
+      </PlayList>
+    </List>
+  </Container>
+);
+
+export default Browse;
